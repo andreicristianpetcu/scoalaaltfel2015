7f10e384d419bac96457056ee98734dbc0d0410d
Andrei Cristian Petcu <andrei@ceata.org>
2015-04-05 03:39:38 +0300

    elevi

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -39,8 +39,49 @@ angular
   var app = angular.module('scoalaaltfel2015App');
   app.run(function ($rootScope, SweetAlert) {
       $rootScope.$on('$routeChangeSuccess', function () {
-          $rootScope.test = 'Global'; 
-          $rootScope.SweetAlert = SweetAlert; 
-          $rootScope.sweetAlert = SweetAlert; 
+          $rootScope.test = 'Global';
+          $rootScope.SweetAlert = SweetAlert;
+          $rootScope.sweetAlert = SweetAlert;
+          $rootScope.elevi = [];
+          $rootScope.adaugaElev = function(){
+            swal({
+              title: "An input!",
+              text: "Write something interesting:",
+              type: "input",
+              showCancelButton: true,
+              closeOnConfirm: false,
+              animation: "slide-from-top"
+            },
+            function(inputValue){
+              if (inputValue === false) return false;
+
+              if (inputValue === "") {
+                swal.showInputError("You need to write something!");
+                return false
+              }
+
+              $rootScope.elevi.push(inputValue);
+              swal("Nice!", "You wrote: " + inputValue, "success");
+              $rootScope.$apply();
+            });
+          }
+          $rootScope.salut2 = function(){
+            swal("Good job!", "You clicked the button!", "success");
+          }
+
+          $rootScope.salut = function(){
+            swal({
+              title: "Are you sure?",
+              text: "You will not be able to recover this imaginary file!",
+              type: "warning",
+              showCancelButton: true,
+              confirmButtonColor: "#DD6B55",
+              confirmButtonText: "Yes, delete it!",
+              closeOnConfirm: false
+            },
+            function(){
+              swal("Deleted!", "Your imaginary file has been deleted.", "success");
+            });
+          }
       });
   });
